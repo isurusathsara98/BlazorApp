667a58dcc857d16cb6d62797b91965f06cc836ff
Isuru Sathsara <Isuru.Sa@CreativeSoftware.com>
2024-01-15 01:27:27 +0530

    Adding inventory page and add inital sketch of methods

---
+++ src/Model/InventoryItem.cs
@@ -0,0 +1,12 @@
+﻿namespace BeautyWeb.Model
+{
+    public class InventoryItem
+    {
+        public string productName {  get; set; }
+        public int? Quantity { get; set; } = null;
+        public int? Price { get; set; } = null;
+        public string Type { get; set; }
+        public int? Discount { get; set; } = null;
+        public string Brand { get; set; }
+    }
+}
---
+++ src/Pages/ProductInventory.razor.cs
@@ -0,0 +1,36 @@
+﻿using BeautyWeb.Model;
+using Microsoft.JSInterop;
+
+namespace BeautyWeb.Pages
+{
+    public partial class ProductInventory
+    {
+        private List<InventoryItem> items = new List<InventoryItem>();
+        private string modalStyle = "display:none;";
+        private InventoryItem newProduct = new InventoryItem();
+        protected override async Task OnInitializedAsync()
+        {
+            items = await JS.InvokeAsync<List<InventoryItem>>("getInventory");
+        }
+
+        private void EditProduct(InventoryItem item) 
+        {
+        }
+        private void DeleteProduct(InventoryItem item) 
+        { 
+        }
+        private void AddProduct() 
+        {
+            modalStyle = "display:block;";
+        }
+        private void CloseAddProductModal()
+        {
+            modalStyle = "display:none;";
+            newProduct = new InventoryItem();
+        }
+        private void AddProductToDb()
+        {
+            
+        }
+    }
+}
